958406712ddd7bf52df1a0d4f4de6d2627e77285
Tom Vander Aa <vanderaa@imec.be>
2021-06-25 16:05:26 +0200

    WIP: windows support

---
+++ recipe/bld.bat
@@ -0,0 +1,26 @@
+mkdir build
+if errorlevel 1 exit 1
+
+cd build
+if errorlevel 1 exit 1
+
+cmake.exe ^
+    -G "%CMAKE_GENERATOR%" ^
+    %CMAKE_ARGS% ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DAF_BUILD_CPU=%ENABLE_CPU% ^
+    -DUSE_CPU_MKL=ON ^
+    -DAF_BUILD_CUDA=%ENABLE_CUDA% ^
+    -DAF_WITH_CUDNN=ON ^
+    -DAF_BUILD_OPENCL=OFF ^
+    -DAF_BUILD_UNIFIED=OFF ^
+    -DAF_BUILD_EXAMPLES=OFF ^
+    -DBUILD_TESTING=OFF ^
+    ${SRC_DIR}
+if errorlevel 1 exit /b 1
+
+cmake.exe --build . --config Release
+if errorlevel 1 exit /b 1
+
+cmake.exe --build . --config Release --target install
+if errorlevel 1 exit /b 1
