ce4f7088c1ae10991186decac857bcc459d60716
Tom Vander Aa <vanderaa@imec.be>
2021-06-29 15:50:37 +0200

    FIX: MKL_ThreadingLibrary empty string

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,13 +10,14 @@ cmake.exe ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
     -DAF_BUILD_CPU=%ENABLE_CPU% ^
     -DUSE_CPU_MKL=ON ^
-    -DMKL_ThreadingLibrary_LINK_LIBRARY= ^
+    -DMKL_ThreadingLibrary_LINK_LIBRARY=" " ^
     -DAF_BUILD_CUDA=%ENABLE_CUDA% ^
     -DAF_WITH_CUDNN=ON ^
     -DAF_BUILD_OPENCL=OFF ^
     -DAF_BUILD_UNIFIED=OFF ^
     -DAF_BUILD_EXAMPLES=OFF ^
     -DBUILD_TESTING=OFF ^
+    -Wno-dev ^
     %SRC_DIR%
 if errorlevel 1 exit /b 1
 
